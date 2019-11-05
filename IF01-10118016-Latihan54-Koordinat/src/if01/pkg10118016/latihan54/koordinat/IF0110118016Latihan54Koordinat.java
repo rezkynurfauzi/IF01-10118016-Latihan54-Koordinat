01c9e44bbd72e18f5faa0b670628fc8179f378df
Rezky Nur Fauzi <55779378+rezkynurfauzi@users.noreply.github.com>
2019-11-05 09:53:09 +0700

    Add files via upload

---
+++ IF01-10118016-Latihan54-Koordinat/src/if01/pkg10118016/latihan54/koordinat/IF0110118016Latihan54Koordinat.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118016.latihan54.koordinat;
+
+/**
+ *
+ * @author Rezky
+ * NAMA      : Rezky Nur Fauzi
+ * KELAS     : IF-1
+ * NIM       : 10118016
+ * Deskripsi Program   : Program ini berisi program Koordinat
+ */
+
+public class IF0110118016Latihan54Koordinat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WarnaKoordinat warnaKoordinat = new WarnaKoordinat(10,4,"Jingga");
+        System.out.println("Warna Koordinat : "+warnaKoordinat.getNamaWarna());
+        System.out.println("Koordinat Sumbu x : "+warnaKoordinat.getX()+", y : "+warnaKoordinat.getY());
+    }
+    
+}
---
+++ IF01-10118016-Latihan54-Koordinat/src/if01/pkg10118016/latihan54/koordinat/Koordinat.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118016.latihan54.koordinat;
+
+/**
+ *
+ * @author Rezky
+ */
+public class Koordinat {
+    protected int x;
+    protected int y;
+
+    public Koordinat(int x, int y) {
+        setX(x);
+        setY(y);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
